21c8350f43361e3960ef8c52f9a39a03d2ea7177
sayken <sayken2000@gmail.com>
2024-02-24 17:26:36 +0900

    CommandLinkを単独のcsに分離

--- Runtime/UIPanel/UIBasePresenter.cs
+++ Runtime/UIPanel/UIBasePresenter.cs
@@ -82,43 +82,4 @@ namespace UuIiView
 
         }
     }
-
-    public class CommandLink
-    {
-        public string Id;
-        public string EventName;
-        public UuIiView.EventType EventType;
-        public bool IsOn;
-        public string PanelName;
-        public Dictionary<string, string> param;
-
-        public CommandLink(string commandLink)
-        {
-            var arr = commandLink.Split("/");
-            PanelName = arr[0];
-            EventName = arr[1];
-            EventType = (UuIiView.EventType)Enum.Parse(typeof(UuIiView.EventType), arr[2]);
-            IsOn = bool.Parse(arr[3]);
-            Id = (arr.Length >= 5) ? arr[4] : "";
-            param = new Dictionary<string, string>();
-            for (int i = 5; i < arr.Length; i++)
-            {
-                var sep = arr[i].Split("=");
-                param[sep[0]] = sep[1];
-            }
-        }
-
-        public string Log()
-        {
-            var paramStr = "";
-            foreach (var kv in param)
-            {
-                paramStr += $",{kv.Key}={kv.Value}";
-            }
-            if (paramStr.Length > 0)
-                paramStr = paramStr.Substring(1);
-
-            return ($"<color=cyan>[UuIiView] CommandLink (Id = {Id} : PanelName={PanelName} : EventName={EventName} : EventType={EventType} : IsOn={IsOn} : param=({paramStr})</color>");
-        }
-    }
 }
