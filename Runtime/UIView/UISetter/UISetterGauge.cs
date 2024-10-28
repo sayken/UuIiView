5a802241ee45a525f56032f774d94d5fc5a6fad6
sayken <sayken2000@gmail.com>
2024-10-28 23:55:31 +0900

    UISetterGauge追加

---
+++ Runtime/UIView/UISetter/UISetterGauge.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UuIiView;
+
+public class UISetterGauge : UISetter
+{
+    Image image;
+    Image Image
+    {
+        get
+        {
+            image = image ?? GetComponent<Image>();
+            return image;
+        }
+    }
+    public override void Set(object obj)
+    {
+        if ( obj.GetType() != typeof(double) )
+        {
+            Debug.Log("obj type is not double : "+ obj.GetType());
+            return;
+        }
+
+        double value = (double)obj;
+        image.fillAmount = (float)value;
+    }
+}
